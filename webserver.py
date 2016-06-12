6cab0f5fcc1ad42654ddde906c09d59d76301e2b
cornellio <peter.cornell@me.com>
2016-06-12 16:23:53 -0700

    Rename variables

--- webserver.py
+++ webserver.py
@@ -53,7 +53,7 @@ class WebServerHandler(BaseHTTPRequestHandler):
                 output += "<html><body>"
                 output += '''
                     <form method='POST' enctype='multipart/form-data'
-                    action='http://localhost:8080/'><h2>Add New Restaurant:</h2><input name="create_new" type="text"><input type="submit" value="Submit"> </form>
+                    action='http://localhost:8080/'><h2>Add New Restaurant:</h2><input name="add_new_resto" type="text"><input type="submit" value="Submit"> </form>
                     '''
                 output += "</body></html>"
                 self.wfile.write(output)
@@ -76,7 +76,7 @@ class WebServerHandler(BaseHTTPRequestHandler):
                 output += '<h1> Rename %s</h1>' % (restaurant)
                 output += '<form method="POST" enctype="multipart/form-data"'
                 output += 'action="http://localhost:8080/">'
-                output += '<input name="new_name" type="text">'
+                output += '<input name="rename_resto" type="text">'
                 output += '<input name="restaurant_id" type="hidden" value="%s">' % (restaurant_id)
                 output += '<input type="submit" value="Rename"> </form>'
                 output += '</body></html>'
@@ -100,7 +100,7 @@ class WebServerHandler(BaseHTTPRequestHandler):
                 output += '<h1> Delete %s</h1>' % (restaurant)
                 output += '<form method="POST" enctype="multipart/form-data"'
                 output += 'action="http://localhost:8080/">'
-                output += '<input name="restaurant_id_del" type="hidden" value="%s">' % (restaurant_id)
+                output += '<input name="del_resto_by_id" type="hidden" value="%s">' % (restaurant_id)
                 output += 'Delete this restaurant?'
                 output += '<p> <input type="submit" value="Go for it!"> </form>'
                 output += '''<input type="button" onclick="location.href='http://localhost:8080/restaurants';" value="Cancel" />'''
@@ -164,9 +164,9 @@ class WebServerHandler(BaseHTTPRequestHandler):
                     print output
 
                 # Add restaurant
-                if fields.get('create_new'):
+                if fields.get('add_new_resto'):
                     session = DBSession()
-                    restaurante_name = fields.get('create_new')
+                    restaurante_name = fields.get('add_new_resto')
                     new_restaurant = Restaurant(name = restaurante_name[0])
                     session.add(new_restaurant)
                     session.commit()
@@ -186,11 +186,11 @@ class WebServerHandler(BaseHTTPRequestHandler):
                     # print output
 
                 # Edit restaurant
-                if fields.get('new_name'):
+                if fields.get('rename_resto'):
                     print 'Edit resto'
                     print 'Got POST fields: ', fields
                     id_current = fields.get('restaurant_id')
-                    name_new = fields.get('new_name')
+                    name_new = fields.get('rename_resto')
                     print id_current
                     print name_new
 
@@ -202,10 +202,10 @@ class WebServerHandler(BaseHTTPRequestHandler):
                     session.add(new)
                     session.commit()
 
-                if fields.get('restaurant_id_del'):
+                if fields.get('del_resto_by_id'):
                     print 'Delete resto:'
                     print 'Got POST fields: ', fields
-                    id = fields.get('restaurant_id_del')
+                    id = fields.get('del_resto_by_id')
 
                     session = DBSession()
 
