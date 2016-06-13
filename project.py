915d06e7ee459c0c818983e379ab67953fd9baaa
cornellio <peter.cornell@me.com>
2016-06-13 16:37:52 -0700

    Add listing of menu items via restaurant_id in url

--- project.py
+++ project.py
@@ -16,11 +16,25 @@ session = DBSession()
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/hello')
-def helloworld():
-    return 'Hello World'
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
 
+    restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+    items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id)
+
+    # output = items.name
+    output = ''
+    for item in items:
+        output += item.name
+        output += '</br>'
+        output += item.price
+        output += '</br>'
+        output += item.description
+        output += '</br></br>'
+
+
+    return output
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host = '0.0.0.0', port = 80)
+    app.run(host = '0.0.0.0', port = 8080)
